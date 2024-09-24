970eec715547198d5e49ea9a6d39e0574126c544
Apelsin88 <143607654+Apelsin88@users.noreply.github.com>
2024-09-24 08:09:08 +0200

    En controller

---
+++ Google Ads - PPC Dashboard/Controllers/ApplicationUserController.cs
@@ -0,0 +1,23 @@
+﻿using Google_Ads___PPC_Dashboard.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Google_Ads___PPC_Dashboard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private readonly ApplicationUserService _applicationUserService;
+
+        public ApplicationUserController(ApplicationUserService applicationUserService)
+        {
+            _applicationUserService = applicationUserService;
+        }
+
+
+
+
+
+    }
+}
