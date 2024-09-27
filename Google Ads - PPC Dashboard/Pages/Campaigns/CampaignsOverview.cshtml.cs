1a285cd6fa89a77661136199ff40360bfa4f3ca9
Apelsin88 <143607654+Apelsin88@users.noreply.github.com>
2024-09-27 17:02:50 +0200

    Tankt Campanjsida test

--- Google Ads - PPC Dashboard/Pages/Campaigns/CampaignsOverview.cshtml.cs
+++ Google Ads - PPC Dashboard/Pages/Campaigns/CampaignsOverview.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Google_Ads___PPC_Dashboard.Models;
 using Google_Ads___PPC_Dashboard.Services;
+using Google.Api;
 
 namespace Google_Ads___PPC_Dashboard.Pages.Campaigns
 {
@@ -10,17 +11,16 @@ namespace Google_Ads___PPC_Dashboard.Pages.Campaigns
     {
         private readonly Google_Ads___PPC_Dashboard.Services.ICampaignService? CampaignService;
 
-        public IList<Campaign> Campagins { get; set; } = default!;
+        public IList<Campaign> Campaigns { get; set; } = default!;
 
         public CampaignsOverviewModel(ICampaignService campaginService)
         {
             CampaignService = campaginService;
         }
 
-        
         public async Task OnGetAsync()
         {
-            Campagins = (IList<Campaign>)await CampaignService.GetAllCampaignsAsync();
+            Campaigns = (IList<Campaign>)await CampaignService.GetAllCampaignsAsync();
         }
     }
 }
